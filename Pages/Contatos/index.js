196a730a3d4358f26cfeebc1acd868d348020350
Matheus <matheus.fusca.lima@outlook.com>
2020-11-26 09:43:55 -0300

    iniciado

---
+++ Pages/Contatos/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container : {
+ 
+  }
+)}
+
+const Contatos = () => {
+    return(
+        <View>
+            <Text>Contatos</Text>
+        </View>
+    )
+}
+
+export default Contatos;
